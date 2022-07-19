7b1cc7fdb678f1950bb196108b937e99ac0887f1
aharoni <aharoniyovel4@gmail.com>
2022-07-19 19:01:05 +0300

    sd

---
+++ src/services/apiService.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const API_URL = "http://localhost:3001";
+// export const API_URL = "https://mysite.co.il";
+
+export const TOKEN_NAME = "donates_tok"
+
+export const doApiGet = async (_url) => {
+
+    try {
+        let resp = await axios.get(_url, {
+            headers: {
+                "x-api-key": localStorage[TOKEN_NAME],
+                "content-Type": "application/json"
+            }
+        })
+        return resp;
+    }
+
+    catch (err) {
+        throw (err);
+    }
+}
+
+// For Post, Delete, Put , Patch
+export const doApiMethod = async (_url, _method, _body = {}) => {
+    try {
+        let resp = await axios({
+            url: _url,
+            method: _method,
+            data: JSON.stringify(_body),
+            headers: {
+                "x-api-key": localStorage[TOKEN_NAME],
+                "content-Type": "application/json"
+            }
+        })
+        return resp;
+    }
+
+    catch (err) {
+        throw (err);
+    }
+}
