5579bde99d44d2d392c4e9badfdec144143616ae
aharoni <aharoniyovel4@gmail.com>
2022-07-19 18:44:14 +0300

    asd

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AppRoutes from './appRoutes';
+
+function App() {
+  return (
+    <React.Fragment>
+      <AppRoutes />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/appRoutes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HeaderClient from './comps/headerClient';
+import Home from './comps/home';
+import About from './comps/about';
+
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+
+            <HeaderClient />
+
+            <Routes>
+
+                <Route index element={<Home />} />
+                <Route path='/about' element={<About />} />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
