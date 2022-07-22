00df285df2a6c060b5877c8247a69ed0df716515
aharoni <aharoniyovel4@gmail.com>
2022-07-22 16:04:45 +0300

    asd

--- src/donates_comps/donItem.jsx
+++ src/donates_comps/donItem.jsx
@@ -7,10 +7,8 @@ export default function DonItem({ item }) {
                 <h2>name: {item.fullName}</h2>
                 <li>short id : {item.short_id}</li>
                 <li>role: {item.role}</li>
-                <li>email: {item.email}</li>
                 <li>city: {item.city}</li>
                 <li>phone: {item.phone}</li>
-                <li>radius: {item.radius}</li>
                 <li>rangePeople: {item.rangePeople}</li>
                 <li>anonymous? : {item.anonymous ? "Yes" : "No"} </li>
             </ul>
