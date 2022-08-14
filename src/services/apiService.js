495e5a6c6958ac67435e9f55f1dca23f9f47b739
aharoni <aharoniyovel4@gmail.com>
2022-08-14 18:50:00 +0300

    שכג

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // export const API_URL = "http://localhost:3001/";
-export const API_URL = "https://foodstack-23.herokuapp.com/";
+// export const API_URL = "https://foodstack-23.herokuapp.com/";
+export const API_URL = "https://aharoni-test.herokuapp.com/";
+
 
 export const EMPLOYEES = "employees";
 export const DONATES = "donates";
