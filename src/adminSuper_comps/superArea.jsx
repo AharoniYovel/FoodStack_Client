369d1dba8825c8ff193f5f3f88ff27fc3059f5fa
aharoni <aharoniyovel4@gmail.com>
2022-08-21 18:05:06 +0300

    sadsa

--- src/adminSuper_comps/superArea.jsx
+++ src/adminSuper_comps/superArea.jsx
@@ -25,8 +25,6 @@ export default function SuperArea() {
                     <div className='container'>
                         <div className='row'>
                             <Trafic />
-                            <Trafic />
-                            <Trafic />
                         </div>
                     </div>
                 </div>
