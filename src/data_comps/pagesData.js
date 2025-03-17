9406ca3afac8df714555327226be5c3d9e98babe
Aharoni yovel <aharoniyovel4@gmail.com>
2025-03-18 00:17:42 +0200

    fix_state

---
+++ src/data_comps/pagesData.js
@@ -0,0 +1,29 @@
+export const links = [
+  {
+    name: "Home",
+    url: "/employee",
+  },
+  {
+    name: "Volunteers list",
+    url: "/employee/volList",
+  },
+  {
+    name: "Donates list",
+    url: "/employee/donList",
+  },
+  {
+    name: "Paths list",
+    url: "/employee/pathList",
+  },
+];
+
+export const settings = [
+  {
+    name: "Profile",
+    url: "/employee",
+  },
+  {
+    name: "Logout",
+    url: "/empLogOut",
+  },
+];
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,7 +4,6 @@ import { infConect } from "../config/secret";
 // export const API_URL = "http://localhost:3001/";
 export const API_URL = infConect.apiUrl;
 
-
 export const EMPLOYEES = "employees";
 export const DONATES = "donates";
 export const VOLUNTEERS = "volunteers";
@@ -15,40 +14,35 @@ export const TOKEN_NAME = infConect.tokenName;
 
 //* For [GET] Request
 export const doApiGet = async (_url) => {
-
-    try {
-        let resp = await axios.get(_url, {
-            headers: {
-                "x-api-key": localStorage[TOKEN_NAME],
-                "content-Type": "application/json"
-            }
-        })
-        return resp;
-    }
-
-    catch (err) {
-        console.log("problem to get data")
-        throw (err);
-    }
-}
+  try {
+    let resp = await axios.get(_url, {
+      headers: {
+        "x-api-key": localStorage[TOKEN_NAME],
+        "content-Type": "application/json",
+      },
+    });
+    return resp;
+  } catch (err) {
+    console.log("problem to get data");
+    throw err;
+  }
+};
 
 //*todo For Post, Delete, Put , Patch
 export const doApiMethod = async (_url, _method, _body = {}) => {
-    try {
-        let resp = await axios({
-            url: _url,
-            method: _method,
-            data: JSON.stringify(_body),
-            headers: {
-                "x-api-key": localStorage[TOKEN_NAME],
-                "content-Type": "application/json"
-            }
-        })
-        return resp;
-    }
-
-    catch (err) {
-        console.log("problem to post/put/del data")
-        throw (err);
-    }
-}
+  try {
+    let resp = await axios({
+      url: _url,
+      method: _method,
+      data: JSON.stringify(_body),
+      headers: {
+        "x-api-key": localStorage[TOKEN_NAME],
+        "content-Type": "application/json",
+      },
+    });
+    return resp;
+  } catch (err) {
+    console.log("problem to post/put/del data");
+    throw err;
+  }
+};
